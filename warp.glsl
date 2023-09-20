a086f71a39b53717dcbc8e4483e58d2ffc4bcff7
Celestin de Villa <plasmacel@gmail.com>
2023-09-20 21:35:00 +0200

    Update warp.glsl

--- warp.glsl
+++ warp.glsl
@@ -1,3 +1,5 @@
+// Template Helpers
+
 #define INSTANTIATE_FLOAT_TEMPLATES(TEMPLATE) 	\
 TEMPLATE(float)					\
 TEMPLATE(vec2)					\
@@ -41,16 +43,19 @@ TEMPLATE(umat2)					\
 TEMPLATE(umat3)					\
 TEMPLATE(umat4)					\
 
-// for GLSL
+// Types
 
+#define short int
 #define int16_t int
 #define int32_t int
 #define int64_t
 
+#define ushort uint
 #define uint16_t uint
 #define uint32_t uint
 #define uint64_t
 
+#define half float
 #define float16_t float
 #define float32_t float
 #define float64_t double
@@ -60,79 +65,105 @@ TEMPLATE(umat4)					\
 #define bool3 bvec3
 #define bool4 bvec4
 
+#define short1 int
+#define short2 ivec2
+#define short3 ivec3
+#define short4 ivec4
+#define short2x2 imat2x2
+#define short2x3 imat3x2
+#define short2x4 imat4x2
+#define short3x2 imat2x3
+#define short3x3 imat3x3
+#define short3x4 imat4x3
+#define short4x2 imat2x4
+#define short4x3 imat3x4
+#define short4x4 imat4x4
+
 #define int1 int
 #define int2 ivec2
 #define int3 ivec3
 #define int4 ivec4
+#define int2x2 imat2x2
+#define int2x3 imat3x2
+#define int2x4 imat4x2
+#define int3x2 imat2x3
+#define int3x3 imat3x3
+#define int3x4 imat4x3
+#define int4x2 imat2x4
+#define int4x3 imat3x4
+#define int4x4 imat4x4
+
+#define ushort1 uint
+#define ushort2 uvec2
+#define ushort3 uvec3
+#define ushort4 uvec4
+#define ushort2x2 umat2x2
+#define ushort2x3 umat3x2
+#define ushort2x4 umat4x2
+#define ushort3x2 umat2x3
+#define ushort3x3 umat3x3
+#define ushort3x4 umat4x3
+#define ushort4x2 umat2x4
+#define ushort4x3 umat3x4
+#define ushort4x4 umat4x4
 
 #define uint1 uint
 #define uint2 uvec2
 #define uint3 uvec3
 #define uint4 uvec4
+#define uint2x2 umat2x2
+#define uint2x3 umat3x2
+#define uint2x4 umat4x2
+#define uint3x2 umat2x3
+#define uint3x3 umat3x3
+#define uint3x4 umat4x3
+#define uint4x2 umat2x4
+#define uint4x3 umat3x4
+#define uint4x4 umat4x4
 
-#define half float
 #define half1 float
 #define half2 float2
 #define half3 float3
 #define half4 float4
+#define half2x2 mat2x2
+#define half2x3 mat3x2
+#define half2x4 mat4x2
+#define half3x2 mat2x3
+#define half3x3 mat3x3
+#define half3x4 mat4x3
+#define half4x2 mat2x4
+#define half4x3 mat3x4
+#define half4x4 mat4x4
 
 #define float1 float
 #define float2 vec2
 #define float3 vec3
 #define float4 vec4
+#define float2x2 mat2x2
+#define float2x3 mat3x2
+#define float2x4 mat4x2
+#define float3x2 mat2x3
+#define float3x3 mat3x3
+#define float3x4 mat4x3
+#define float4x2 mat2x4
+#define float4x3 mat3x4
+#define float4x4 mat4x4
 
 #define double1 double
 #define double2 dvec2
 #define double3 dvec3
 #define double4 dvec4
-
-#define float2x2 mat2x2
-#define float2x3 mat3x2
-#define float2x4 mat4x2
-
-#define float3x2 mat2x3
-#define float3x3 mat3x3
-#define float3x4 mat4x3
-
-#define float4x2 mat2x4
-#define float4x3 mat3x4
-#define float4x4 mat4x4
-
 #define double2x2 dmat2x2
 #define double2x3 dmat3x2
 #define double2x4 dmat4x2
-
 #define double3x2 dmat2x3
 #define double3x3 dmat3x3
 #define double3x4 dmat4x3
-
 #define double4x2 dmat2x4
 #define double4x3 dmat3x4
 #define double4x4 dmat4x4
 
-#define int2x2 imat2x2
-#define int2x3 imat3x2
-#define int2x4 imat4x2
-
-#define int3x2 imat2x3
-#define int3x3 imat3x3
-#define int3x4 imat4x3
-
-#define int4x2 imat2x4
-#define int4x3 imat3x4
-#define int4x4 imat4x4
-
-#define uint2x2 umat2x2
-#define uint2x3 umat3x2
-#define uint2x4 umat4x2
-
-#define uint3x2 umat2x3
-#define uint3x3 umat3x3
-#define uint3x4 umat4x3
-
-#define uint4x2 umat2x4
-#define uint4x3 umat3x4
-#define uint4x4 umat4x4
+// Intrinsic Functions
 
 #define atan2(a, b) \
 atan(a, b)
